311f5ce1ec3ad72e6796c80cac67001e4b9847e3
Diego <dfreile51@gmail.com>
2021-11-08 12:59:15 +0100

    0745.02 Añadido el metodo vaciarDineroDeLaMaquina y comprobado que funciona

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -85,4 +85,11 @@ public class MaquinaExpendedoraMejorada {
         balanceClienteActual = 0;
         return cantidadDeDineroADevolver;
     } 
+    
+    public int vaciarDineroDeLaMaquina() {
+        int totalDineroExtraido = balanceClienteActual + totalDineroAcumulado;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        return totalDineroExtraido;
+    }
 }
